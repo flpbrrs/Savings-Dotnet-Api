62df6c493af9f4ce4b15c9d3659529dfef874317
Felipe Jonathan <felipejonathan.dev@gmail.com>
2025-11-16 10:40:00 -0300

    test: aprimorando os testes com a lib Bogus para criação de dados fake

---
+++ tests/CommonUtilitiesTest/Request/RegisterExpenseRequestJsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Savings.Comunication.Requests;
+using Bogus;
+using Savings.Comunication.Enums;
+
+namespace CommonUtilitiesTest.Request;
+
+public class RegisterExpenseRequestJsonBuilder
+{
+    public static RegisterExpenseRequestJson Build()
+    {
+        return new Faker<RegisterExpenseRequestJson>()
+            .StrictMode(true)
+            .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+            .RuleFor(r => r.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(r => r.Amount, f => f.Finance.Amount())
+            .RuleFor(r => r.Date, f => f.Date.Past())
+            .RuleFor(r => r.ExpenseType, f => f.PickRandom<ExpenseType>());
+    }
+}
